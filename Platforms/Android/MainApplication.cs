2a73b1b8e0aa3aa0a9b3781dc56039c47e8b4b7c
PreyanshuShah <165466380+PreyanshuShah@users.noreply.github.com>
2024-12-17 09:14:53 +0545

    Add project files.

---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace CW1Preyanshu
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
